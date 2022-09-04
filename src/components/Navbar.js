18a3a0041df5f49a1887c5aa063c6ad5f078c54b
Rodrigo Bossini <professorbossini@gmail.com>
2022-09-04 16:56:47 -0300

    navbar: titulo azul

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = props => {
           <img src={logo} alt="logo" width={150} />
         </div>
       </div>
-      <div className="text-900 font-bold text-4xl mb-4 text-center">INSTITUTO MAUÁ DE TECNOLOGIA</div>
+      <div className="text-900 font-bold text-4xl mb-4 text-center text-indigo-800">INSTITUTO MAUÁ DE TECNOLOGIA</div>
       <div className="text-700 text-xl mb-6 text-center line-height-3">Ranking do programa de monitoria</div>
     </div>
   )
